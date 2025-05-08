944e2345943942942c724e59ba68ea350e293035
Maria Fernanda Zúñiga González <mafecitazugo@hotmail.com>
2025-05-07 19:31:24 -0500

    Incluí el lulo en la mitad y cambié el script.src=

---
+++ Pelar.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    fill(154, 205, 50); // Yellow-green color
+    ellipse(width / 2, height / 2, 200, 200); // Draws a circle at the center
+}
